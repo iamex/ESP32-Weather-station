463df7c794817d6cd924df2995a7bfdaa99e4b50
Giacomo Mancin <iemes514@gmail.com>
2024-07-04 17:24:48 +0200

    Update main.cpp

--- ESP32_OpenWeatherMap/src/main.cpp
+++ ESP32_OpenWeatherMap/src/main.cpp
@@ -1,8 +1,8 @@
 /*                      OpenWheatherMap_ESP32                   *
  *                      =====================                   *
  *                                                              *
- * Code to query the OpenWheatherMap service which              * 
- * provides weather forecast in JSON format                     *
+ *   ESP32 weather station that queries the OpenWeatherMap      *
+ *   service and displays the retrieved data on a web page.     *
  *                                                              *
  * Author: Giacomo Mancin                                       *
  *                                                              */
